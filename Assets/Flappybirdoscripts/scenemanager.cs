59f2d78b9cce24243f1b07e3d3701d3bf9ed083a
Alonso Pérez <edalopp@hotmail.com>
2026-05-29 19:19:11 -0500

    intro buttons

---
+++ Assets/Flappybirdoscripts/scenemanager.cs
@@ -0,0 +1,24 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenemanager : MonoBehaviour
+{
+    public string sceneName;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
